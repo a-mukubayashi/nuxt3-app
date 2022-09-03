3d4c279190f8b7ccce9f73d1544bb95a6f5b5df9
a-mukubayashi <akawasumi92@gmail.com>
2022-09-03 19:23:23 +0900

    feat: components and pages

---
+++ src/pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <main>
+    <h1>Index page</h1>
+    <NuxtLink to="/welcome">welcome page</NuxtLink>
+  </main>
+</template>
+
+<script setup>
+definePageMeta({
+  title: "index page title",
+});
+</script>
---
+++ src/pages/welcome.vue
@@ -0,0 +1,8 @@
+<template>
+  <main>
+    <h1>Welcome page</h1>
+    <NuxtLink to="/">index page</NuxtLink>
+    <br />
+    <UiButton />
+  </main>
+</template>
